81d45d08bb0d1486447db4fd0701b6e675c60fac
Heneros <punk60826@gmail.com>
2021-07-30 23:58:18 +0300

    get data

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,12 @@ import axios  from 'axios';
 
 
 const Search = () =>{
-   const [term, setTerm ] = useState('');
+   const [term, setTerm ] = useState('programming');
+   const [results, setResults] = useState([]);
 
    useEffect(() =>{
     const search = async () =>{
-        await axios.get('https://en.wikipedia.org/w/api.php', {
+      const {data}  = await axios.get('https://en.wikipedia.org/w/api.php', {
             params:{
                 action: 'query',
                 list: 'search',
@@ -16,6 +17,8 @@ const Search = () =>{
                 srsearch: term
             }
         });
+
+        setResults(data);
     };
 
     search();
