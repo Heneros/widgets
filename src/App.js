8f414313a3509cf1237206b2922fa4332248cc4b
Heneros <punk60826@gmail.com>
2021-08-03 02:57:21 +0300

    dropdown file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import Accordion  from './components/Accordion';
-import Search  from './components/Search';
+import React       from 'react';
+import Accordion   from './components/Accordion';
+import Search      from './components/Search';
+import Dropdown    from './components/Dropdown';
 
 
 const items = 
@@ -17,12 +18,28 @@ const items =
 },
 ];
 
+const options = [
+  { 
+      label: 'The color Red',
+      value: 'red'
+  },
+  { 
+    label: 'The color green',
+    value: 'green'
+},
+{ 
+    label: 'The color blue',
+    value: 'blue'
+},
+];
+
 
 export default () =>{ 
     return(
         <div>
              {/* <Accordion items={items} /> */}
-             <Search />
+             {/* <Search /> */}
+             <Dropdown options={options} />
          </div>   
     )
 }
