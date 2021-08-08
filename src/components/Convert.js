513985ead52f87fee4046a5089025d79dc2c7df6
Heneros <punk60826@gmail.com>
2021-08-08 22:55:06 +0300

    connected api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const Convert = ({language, text}) =>{
     useEffect(() =>{
-        console.log("new language");
+        axios.post(
+            'https://translation.googleapis.com/language/translate/v2',
+            {},
+            {
+              params: {
+                q: text,
+                target: language.value,
+                key: '',
+              },
+            }
+          );
     }, [language, text]);
     return <div/>
 }
