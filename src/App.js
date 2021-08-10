9d4498227b34b46c3b3b28d72fbf8bc1d4754275
Heneros <punk60826@gmail.com>
2021-08-10 03:00:34 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -33,24 +33,36 @@ const options = [
 },
 ];
 
+const showAccordion = () =>{
+   if(window.location.pathname === '/'){
+       return <Accordion items={items} />;
+   }
+};
+const showList = () =>{
+    if(window.location.pathname === '/list'){
+        return <Search/>;
+    }
+ };
 
+ const showDropdown = () =>{
+    if(window.location.pathname === '/dropdown'){
+        return <Dropdown/>;
+    }
+ };
+ const showTranslate = () =>{
+    if(window.location.pathname === '/translate'){
+        return <Translate/>;
+    }
+ };
+ 
 export default () =>{ 
-    // const [selected, setSelected] = useState(options[0]);
-    // const [showDropdown, setShowDropdown] = useState(true);
+
     return(
         <div>
-            {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button> */}
-             {/* <Accordion items={items} /> */}
-             {/* <Search /> */}
-          {/* {  showDropdown ?
-           <Dropdown 
-             selected={selected}
-             onSelectedChange={setSelected}
-              options={options} />
-
-              : null } */}
-
-              <Translate />
+              {showAccordion()}
+              {showList()}
+              {showDropdown()}
+              {showTranslate()}
          </div>   
     )
 }
